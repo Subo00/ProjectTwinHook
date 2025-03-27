2d5f90a55071688e4622784e40474faca3da573f
Ernest Subotić <gamedevsubo@gmail.com>
2025-03-27 15:26:46 +0100

    Add Ernest folder for testing purpouses

---
+++ Assets/Ernest/InteractableChild.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableChild : Interactable
+{
+    protected override void OnUpdate(){
+        CommonLogic();
+    }
+}
