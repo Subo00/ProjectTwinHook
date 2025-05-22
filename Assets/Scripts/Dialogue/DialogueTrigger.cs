f6c6d10d8d2cb590136d4c2a635c10fc43589c8a
Ernest Subotić <gamedevsubo@gmail.com>
2025-05-22 10:59:00 +0200

    Update DIalogueTrigger

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -4,13 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 
+[RequireComponent(typeof(BoxCollider))]
 public class DialogueTrigger : MonoBehaviour {
     public DialogueGraph tree;
 
-    bool alreadyPlayed = false;
+    protected bool alreadyPlayed = false;
 
 
-    private void OnTriggerEnter(Collider collision) {
+    protected void OnTriggerEnter(Collider collision) {
+        DoOnEnter(collision);
+    }
+
+    protected virtual void DoOnEnter(Collider collision) {
         if (collision.gameObject.tag == "Player" && !alreadyPlayed) {
             TriggerDialogue();
             alreadyPlayed = true;
