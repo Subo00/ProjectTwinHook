ee0a6e57b185be14d455c60f67578ab205f4dbd0
eyeofthedragon <eileencarette@gmail.com>
2025-05-29 16:06:37 +0200

    music fix

--- Assets/Scripts/Dialogue/DialogueTriggers/DialogueTriggerStart.cs
+++ Assets/Scripts/Dialogue/DialogueTriggers/DialogueTriggerStart.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class DialogueTriggerStart : DialogueTrigger
 {
     public GameObject platform;
+    public AudioSource musicController;
 
     //not effective way of checking things but okay 
     private void Update() {
         if (alreadyPlayed && !DialogueManager.Instance.dialogueIsPlaying) {
             platform.SetActive(false);
+            musicController.Play();
             Destroy(this.gameObject); //we don't need it anymore 
         }
     }
