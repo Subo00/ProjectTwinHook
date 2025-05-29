2ce004501ffba8445a8d195080a05b70c399c359
Ernest Subotić <gamedevsubo@gmail.com>
2025-05-29 20:50:19 +0200

    Add minor changes to the PlayerMovement

--- Assets/Scripts/movement/PlayerMovement.cs
+++ Assets/Scripts/movement/PlayerMovement.cs
@@ -508,7 +508,7 @@ namespace TwinHookController
                     Vector3 spawnPosition = transform.position; // tweak if needed
                     activeAnchor = Instantiate(anchorPrefab, spawnPosition, Quaternion.identity);
                     duckedObject.SetActive(true);
-                    playerCollider.enabled = false;
+                    //playerCollider.enabled = false;
                 }
                 return;
             }
@@ -516,7 +516,7 @@ namespace TwinHookController
             {
                 Destroy(activeAnchor);
                 duckedObject.SetActive(false);
-                playerCollider.enabled = true;
+                //playerCollider.enabled = true;
                 activeAnchor = null;
             }
             if (isFrozen)
